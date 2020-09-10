a48e944c3858a3337379be7ad29fafaa7b91571b
André Loureiro <loureiro.andrew@gmail.com>
2020-09-10 17:55:38 -0400

    Updates

---
+++ precache-manifest.a5fd31e35c08019ed0555cc17a6187bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b243b66d8e29881364276d04eb21a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88852d610715228b0931",
+    "url": "/static/js/2.a7d730d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a7d730d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17dfd3d3a780a06fce9b",
+    "url": "/static/js/main.649fab52.chunk.js"
+  },
+  {
+    "revision": "3eeed2e82ed5ca2e8c5f",
+    "url": "/static/js/runtime-main.77d84e4d.js"
+  }
+]);
